89e855d2aedb1ea78b3df1fed46021e7cb20dab1
Niranjit123 <106467254+Niranjit123@users.noreply.github.com>
2025-04-08 12:31:44 +0530

    add interface pageprops at Blogpost Params

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,37 +1,33 @@
-import {getPostContent, getAllPostSlugs } from '@/lib/markdown'
+import { getPostContent, getAllPostSlugs } from '@/lib/markdown'
 
-export async function generateStaticParams(){
+export async function generateStaticParams() {
     const posts = getAllPostSlugs()
-
-    return posts.map((post)=>({
+    return posts.map((post) => ({
         slug: post.slug,
-
     }))
 }
 
-// eslint-disable-next-line
-
-
-type BlogPostParams = {
+// Use the Next.js specific type definition
+interface PageProps {
   params: {
     slug: string;
   };
-};
+  searchParams?: Record<string, string | string[] | undefined>;
+}
 
-export default async function BlogPost({params}: BlogPostParams){
+export default async function BlogPost({ params }: PageProps) {
+  const { slug } = params
+  const post = await getPostContent(slug)
   
-  const {slug} =  params  
-
-    const post = await getPostContent(slug)
-    return(
-        <div className="max-w-4xl mx-auto py-10 px-4">
-        <h1 className="text-4xl font-bold mb-2">{post.title}</h1>
-        {/* <p className="text-gray-500 mb-8">{post?.date}</p> */}
-        
-        <div 
-          className="prose lg:prose-xl" 
-          dangerouslySetInnerHTML={{ __html: post.contentHtml }} 
-        />
-      </div>
-    )
+  return (
+    <div className="max-w-4xl mx-auto py-10 px-4">
+      <h1 className="text-4xl font-bold mb-2">{post.title}</h1>
+      {/* <p className="text-gray-500 mb-8">{post?.date}</p> */}
+      
+      <div 
+        className="prose lg:prose-xl" 
+        dangerouslySetInnerHTML={{ __html: post.contentHtml }} 
+      />
+    </div>
+  )
 }
